ffa80fbaf80e94ad0499e56186b032705dddb724
haomiao <zhanghm1994@icloud.com>
2017-01-14 16:39:03 +0800

    fix_conflict_ac_cdlatex

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,6 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-auto-show-menu 0.2)
+ '(ac-auto-start 4)
+ '(ac-ispell-fuzzy-limit 2)
+ '(ac-ispell-requires 4)
  '(company-idle-delay 0.08)
  '(company-minimum-prefix-length 1)
  '(custom-safe-themes
